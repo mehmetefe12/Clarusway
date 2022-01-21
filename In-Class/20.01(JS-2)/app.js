7f1b7f9a0cde72417c1cafb85dc7d8fbd14dcff7
mehmetefe <mehmetustundag417@gmail.com>
2022-01-21 10:34:36 +0100

    20.01(JS-2)

---
+++ In-Class/20.01(JS-2)/app.js
@@ -0,0 +1,93 @@
+/* console.log("I'm from file");
+var x = 7;
+var y = 3;
+
+console.log(x);
+console.log({y});
+y = x ; 
+console.log({y});
+console.log(typeof(x))
+console.log(typeof(y)) 
+
+var alert= "Merhaba"
+console.log(alert) */
+
+
+/* let a = "Global"
+console.log(a);
+{
+    let a = "Scope"
+    console.log(a);
+    {
+        let a = "inner scope"
+        console.log(a);
+    }
+}
+
+console.log(a); */
+
+/* let c = 3;
+console.log(c);
+let c = 5;
+console.log(c); */
+
+/* var a = 3;
+var aa = 5; // bur da var olarak atadigimiz da bize window altinda otomatik deger atiyor.
+
+// ama coast oder let te bu bu sekilde hafizada yer kaplamiyor
+console.log(c);
+var c = 6;
+
+console.log(d);
+let e = 5; */
+
+/* var f;
+console.log(typeof f);
+
+f = 5 ;
+console.log( typeof f );
+
+f = "Merhaba ben string";
+console.log( typeof f );
+
+console.log(typeof 5);
+console.log(typeof 5.45);
+console.log(typeof 0);
+console.log(typeof -5);
+console.log(typeof (0/1));
+console.log(typeof NaN);
+console.log(typeof typeof 5);  */
+
+/* var merhaba = 'Merhaba'
+var selam = "Sana da \"selam\""
+
+console.log(merhaba);
+console.log(selam);
+
+
+var instructor = 'Mark'
+var d = `Merhaba ${instructor}`
+console.log(d); */
+
+/* console.log(Boolean(""));
+console.log(Boolean(3<5));
+console.log(Boolean(3>5));
+console.log(Boolean(3 == 5));
+console.log(Boolean(("0")));
+var s;
+console.log(Boolean(s));
+console.log(Boolean(null));
+console.log(typeof s);
+console.log(typeof null);  */
+
+console.log(2 == "2");
+console.log(2 === "2");
+console.log(2 + "2");
+console.log(2 + +"2"); 
+
+
+
+
+
+
+
